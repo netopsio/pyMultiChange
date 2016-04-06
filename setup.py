48ad154c0c913f60f8eb01695340acc4407e7cab
jame4848 <james.williams@rackspace.com>
2016-04-06 05:00:26 +0000

    change netlib dependency to master

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='pymulti_change',
       description='A script to make mass changes to routers and switches.',
       author='James Williams',
       url='https://github.com/jtdub/pyMultiChange',
-      dependency_links = ['http://github.com/jtdub/netlib/tarball/setup'],
+      dependency_links = ['http://github.com/jtdub/netlib/tarball/master'],
       install_requires = ['netlib'],
       scripts=['bin/multi_change.py'])
